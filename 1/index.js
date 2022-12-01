b6463c4a43d01cf09730ef7e0d83a1d07c0383ad
Arne Mæhlum <arne.mahlum@gmail.com>
2022-12-01 15:13:28 +0100

    1.2

--- 1/index.js
+++ 1/index.js
@@ -2,11 +2,14 @@ const fs = require('fs');
 
 const sumTotal = (sum, next) => sum + next;
 const maximize = (max, next) => Math.max(max, next);
+const descending = (one, two) => two - one;
 
 const inputStr = String(fs.readFileSync('./input.txt'));
 
-const maxCarried = inputStr.split(`\n\n`)
+const maxThreeCarryTotal = inputStr.split(`\n\n`)
   .map(elfStr => elfStr.split(`\n`).map(Number).reduce(sumTotal, 0))
-  .reduce(maximize);
+  .sort(descending)
+  .slice(0, 3)
+  .reduce(sumTotal, 0);
 
-console.log(`maxCarried`, maxCarried); // DEBUG
+console.log(`maxThreeCarryTotal`, maxThreeCarryTotal); // DEBUG
