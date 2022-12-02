9f0869149b2ea8fc5512b0a66e9b04aeaa479899
Arne Mæhlum <arne.mahlum@gmail.com>
2022-12-02 14:29:27 +0100

    2.2 Should have worked??
    9458 is too low.

--- 2/index.js
+++ 2/index.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
 const inputStr = String(fs.readFileSync('./input.txt'));
 
+// Data
+const win = 6;
+const tie = 3;
+const loss = 0;
+
 const mapping = {
   A: 1,
   B: 2,
   C: 3,
-  X: 1,
-  Y: 2,
-  Z: 3,
+  X: loss,
+  Y: tie,
+  Z: win,
 }
 
 const rounds = inputStr.split('\n')
@@ -15,15 +20,10 @@ const rounds = inputStr.split('\n')
     round
       .split(' ')
       .map(l => mapping[l])
-      .reverse() // It's them first in the input file
   );
 
 // Logic
-const win = 6;
-const tie = 3;
-const loss = 0;
-
-function outcome(me, them) {
+function getOutcome(me, them) {
   const ans = (3 + me - them) % 3;
   switch(ans) {
     case 1:
@@ -37,12 +37,23 @@ function outcome(me, them) {
   }
 }
 
-function score(me, them) {
-  return me + outcome(me, them);
+function getScore(them, desiredOutcome) {
+  const diff = desiredOutcome / 3 - 1;
+  const me = (them + diff + 3) % 3;
+  const outcome = getOutcome(me, them);
+
+  if (outcome !== desiredOutcome) {
+    throw Error(`Wanted ${desiredOutcome} but got ${outcome}`);
+  } else {
+    // console.log(`outcome`, outcome); // DEBUG
+    // console.log(`score`, me + outcome); // DEBUG
+  }
+
+  return me + outcome;
 }
 
 const sumTotal = (sum, next) => sum + next;
 
-const total = rounds.map(round => score(...round)).reduce(sumTotal, 0);
+const total = rounds.map(([them, outcome]) => getScore(them, outcome)).reduce(sumTotal, 0);
 
 console.log(`total`, total); // DEBUG
