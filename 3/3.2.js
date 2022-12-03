da2a05da834b42e7e406f1ae7506e5a397a02743
Arne Mæhlum <arne.mahlum@gmail.com>
2022-12-03 14:18:48 +0100

    3.2 sample test

---
+++ 3/3.2.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const inputStr = String(fs.readFileSync('./sample_input.txt'));
+
+// Data
+const groups = inputStr.split('\n')
+  .reduce((groups, sack, index) => {
+    if (index % 3 === 0) {
+      return groups.concat({ sacks: [sack] });
+    } else {
+      groups.slice(-1)[0].sacks.push(sack);
+      return groups;
+    }
+  }, []);
+
+// Logic
+const pri = Object.fromEntries(
+  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    .split('')
+    .map((char, index) => [char, index + 1])
+);
+
+function findDuplicate(group) {
+  const [one, two, three] = group.sacks;
+  return one.split('').find(char => two.includes(char) && three.includes(char));
+}
+
+function getPriority(group) {
+  return pri[findDuplicate(group)];
+}
+
+const sumTotal = (sum, next) => sum + next;
+
+const total = groups.map(getPriority).reduce(sumTotal, 0);
+
+console.log(`groups.map(findDuplicate)`, groups.map(findDuplicate)); // DEBUG
+console.log(`groups.map(getPriority)`, groups.map(getPriority)); // DEBUG
+console.log(`total`, total); // DEBUG
