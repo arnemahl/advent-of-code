3d5b3aef7ae7742646e737e7317a4b4006c9d135
Arne Mæhlum <arne.mahlum@gmail.com>
2022-12-10 12:18:24 +0100

    10.1 Done

---
+++ a/index.js
@@ -0,0 +1,84 @@
+// Utils
+const sumTotal = (sum, next) => sum + next;
+
+// Data
+function getData(inputString) {
+  return inputString.split('\n').map(line => {
+    const [cmd, arg] = line.split(' ');
+    return {
+      cmd,
+      arg: arg ? Number(arg) : undefined,
+    }
+  });
+}
+
+// Logic
+function getLog(instructions) {
+  let cycle = 1;
+  let X = 1;
+  let log = [];
+
+  function check() {
+    const shouldCount = (cycle - 20) % 40 === 0;
+    if (shouldCount) {
+      const signalStrength = cycle * X;
+      log.push({ cycle, X, signalStrength });
+    }
+  }
+
+  instructions.forEach(({ cmd, arg }) => {
+    switch (cmd) {
+      case 'noop':
+        cycle++; check();
+        break;
+      case 'addx': {
+        cycle++; check();
+        X += arg;
+        cycle++; check();
+      }
+    }
+  });
+
+  return log;
+}
+
+function getAnswer(instructions) {
+  return getLog(instructions).map(entry => entry.signalStrength).reduce(sumTotal, 0);
+}
+
+
+// Execute
+const fs = require('fs');
+
+// 10.1
+function task_1_test_2() {
+  const data = getData(String(fs.readFileSync('./sample_input_2.txt')));
+  const ans = getAnswer(data);
+
+  console.assert(ans === 13140, 'Task 1 ans wrong:', ans);
+
+  const str = input => JSON.stringify(input, null, 2);
+  const log = getLog(data);
+  const expected = [
+    { cycle: 20, X: 21, signalStrength: 420 },
+    { cycle: 60, X: 19, signalStrength: 1140 },
+    { cycle: 100, X: 18, signalStrength: 1800 },
+    { cycle: 140, X: 21, signalStrength: 2940 },
+    { cycle: 180, X: 16, signalStrength: 2880 },
+    { cycle: 220, X: 18, signalStrength: 3960 },
+  ];
+
+  console.assert(str(log) === str(expected), "Task 1 log wrong:", log);
+}
+
+function task_1() {
+  const data = getData(String(fs.readFileSync('./input.txt')));
+  const ans = getAnswer(data);
+
+  console.log(`Task 1:`, ans);
+}
+
+{
+  task_1_test_2();
+  task_1();
+}
